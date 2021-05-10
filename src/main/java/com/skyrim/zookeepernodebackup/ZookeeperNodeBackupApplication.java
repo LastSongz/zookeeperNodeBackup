ea7d80fcffe5c2e52753d05b335b4b23fafda2eb
宋凯旋 <songkaixuan@189.cn>
2021-05-10 15:40:20 +0800

    Initial commit

---
+++ src/main/java/com/skyrim/zookeepernodebackup/ZookeeperNodeBackupApplication.java
@@ -0,0 +1,13 @@
+package com.skyrim.zookeepernodebackup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperNodeBackupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperNodeBackupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skyrim/zookeepernodebackup/ZookeeperNodeBackupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyrim.zookeepernodebackup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZookeeperNodeBackupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
